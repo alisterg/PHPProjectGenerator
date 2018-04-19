cce19440692c382789bfa27d085323650922446e
alisterg <alister7@gmail.com>
2018-04-19 18:34:31 +1000

    used some ipc to display modal from main process

--- controller/index-renderer.js
+++ controller/index-renderer.js
@@ -1,29 +1,13 @@
 const { BrowserWindow } = require('electron').remote
+const { ipcRenderer } = require('electron')
 const path = require('path')
 const newWindowBtn = document.getElementById('frameless-window')
 
+// change conditions to send the message to create modal...
 newWindowBtn.addEventListener('click', (event) => {
-  createModal()
+  ipcRenderer.send('asynchronous-message', 'newWindowClicked')
 })
 
-// TODO put this in the main process to set mainWindow as the parent for correct modalness
-function createModal(backgroundColor) {
-  // red: "#ef5350" green: #66bb6a
-
-  const modalPath = path.join('file://', __dirname, '../view/alert-modal.html')
-
-  let modalWin = new BrowserWindow({
-    modal: true,
-    // parent: mainWindow,
-    x: 100,
-    y: 100,
-    width: 350,
-    height: 200,
-    frame: false,
-    backgroundColor: backgroundColor
-  })
-
-  modalWin.on('close', () => { modalWin = null })
-  modalWin.loadURL(modalPath)
-  modalWin.show()
-}
+ipcRenderer.on('asynchronous-reply', (event, arg) => {
+  document.getElementById('async-reply').innerHTML = `Reply: ${arg}`
+})
--- controller/main.js
+++ controller/main.js
@@ -1,4 +1,5 @@
 const electron = require('electron')
+const { ipcMain } = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 
@@ -36,4 +37,33 @@ app.on('activate', function () { // OS X
 })
 
 //////// Anciliary main process code here
-//test
+
+ipcMain.on('asynchronous-message', (event, arg) => {
+  if(arg === "newWindowClicked"){
+    createModal("#ef5350")
+  }
+
+  event.sender.send("asynchronous-reply", "created modal successfully")
+})
+
+
+function createModal(backgroundColor) {
+  // red: "#ef5350" green: #66bb6a
+
+  const modalPath = path.join('file://', __dirname, '../view/alert-modal.html')
+
+  let modalWin = new BrowserWindow({
+    modal: true,
+    parent: mainWindow,
+    x: 100,
+    y: 100,
+    width: 350,
+    height: 200,
+    frame: false,
+    backgroundColor: backgroundColor
+  })
+
+  modalWin.on('close', () => { modalWin = null })
+  modalWin.loadURL(modalPath)
+  modalWin.show()
+}
